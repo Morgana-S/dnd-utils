31d8d9b18cc45fab2fde324fcfe87f5aebed48e5
Morgana-S <st35giv9@students.codeinstitute.net>
2025-01-08 12:19:35 +0000

    Refactor functions for code independence & comments

--- run.py
+++ run.py
@@ -26,17 +26,19 @@ def introduction():
     """
     Prints an introductory message and asks the user to select which function they would like to utilize.
     """
+    # Prints an introductory message using methods from colorama for color
     print(f"Welcome to" + Fore.RED + " DNDUtils" + Fore.WHITE + "!")
     print(f"Please choose a program to use:\n")
     print(Fore.YELLOW + "DiceRoller" + Fore.RESET + " - Roll Dice for your DND Game")
     print(Fore.CYAN + "Fluff" + Fore.RESET + " - Create NPCs or Places for your DND Game\n")
+    # Defines the available options for function selection
     functions = [
         Fore.YELLOW + "DiceRoller",
         Fore.CYAN + "Fluff",
         Back.RED + "Exit DNDUtils"
     ]
+    # Asks the user to choose a function from the choices above
     chosen_function = functions[cutie.select(functions)]
-    print(f"You have chosen {chosen_function}")
     return chosen_function
 
 def diceroller():
@@ -44,20 +46,28 @@ def diceroller():
     Rolls a chosen number of dice with a designated number of sides plus a modifier
     and then displays the output
     """
+    # Defines the variables used to calculate the dice rolls with user input - validates for number-only input
     num_of_dice = cutie.get_number("How many dice do you want to roll?", min_value = 1, allow_float = False)
     num_of_sides = cutie.get_number("How many sides should each dice have?", min_value = 2, allow_float = False)
     dice_rolls =[random.randint(1, num_of_sides) for value in range(num_of_dice)]
     modifier_num = cutie.get_number("What is the modifier for the roll?", allow_float = False)
+    # Prints the result
     print(Fore.GREEN + "\nDice Summary")
-    print(f"\nThe individual dice rolls were: {dice_rolls}")
+    print(f"\n\u001b[32mThe individual dice rolls were: {dice_rolls}")
     print(f"The total of all dice is: {sum(dice_rolls)}")
     print(f"The sum of all dice rolls plus the modifier was: {sum(dice_rolls) + modifier_num}")
+    if cutie.prompt_yes_or_no(Fore.YELLOW + "Would you like to roll more dice?"):
+        # Begins the dice-rolling function again
+        diceroller()
+    else:
+        # Loops the program back to the start
+        main() 
 
-def function_selection():
+
+def function_selection(chosen_function):
     """
     Starts the user's chosen function
     """
-    chosen_function = introduction() # Runs the introduction and asks the user to pick a function
     if "DiceRoller" in chosen_function:
         print(f"Starting" + Fore.YELLOW + " DiceRoller" + Fore.RESET + "...")
         diceroller()
@@ -67,6 +77,7 @@ def function_selection():
         print(Back.RED + "Exiting DNDUtils" + Back.RESET + "...")
 
 def main():
-    function_selection()
+    chosen_function = introduction()
+    function_selection(chosen_function)
 
 main()
